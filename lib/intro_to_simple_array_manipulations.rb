9b11f72cd5abf1a04505eb23435c1c51d694bc55
Randall D Williams <randallw1996@gmail.com>
2019-11-12 03:43:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,5 +30,7 @@ def using_insert(list_of_programming_languages , another_language)
   list_of_programming_languages.insert(4, another_language)
 end
 
-def 
+def using_uniq(haircuts)
+  haircuts.uniq 
+end
   
