9031cf067ae0d2becbfdad43951ee47f8f7350ec
Randall D Williams <randallw1996@gmail.com>
2019-11-12 03:14:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,6 +6,6 @@ def using_unshift(bouroughs_in_nyc , new_neighborhood)
   bouroughs_in_nyc.unshift(new_neighborhood)
 end
 
-def deleted_string(continents)
+def using_pop(continents)
   continents.pop
 end
